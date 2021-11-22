9a0a038136fbc44dee2f111ac71fc0bd91ac728f
AndreaConti <andrea.conti23331@gmail.com>
2021-11-22 14:52:35 +0100

    Correzioni

--- src/org/generation/italy/strings/VerificaPalindrome.java
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -8,22 +8,21 @@ public class VerificaPalindrome {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Inserisci una parola: ");
 		String parola = scanner.nextLine();
-		System.out.println("Hai digitato: " + parola);
-		String nparola = "";
+		String parolaInversa = "";
+		System.out.println("Hai digitato: " + parola + ".");
+		
 		char ch;
 
 		for (int i = 0; i < parola.length(); i++) {
 			ch = parola.charAt(i);
-			nparola = ch + nparola;
+			parolaInversa = ch + parolaInversa;
 		}
 
-		if (parola.equals(nparola)) {
-			System.out.println("E' palindromo");
+		if (parola.equals(parolaInversa)) {
+			System.out.println("E' un palindromo");
 		} else {
-			System.out.println("Non è palindromo");
+			System.out.println("Non è un palindromo.");
 		}
-
-		System.out.println(nparola);
 
 		scanner.close();
 	}
